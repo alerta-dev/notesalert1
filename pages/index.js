9c0cfa693b2f38c20a6e1cffaf691229df3ac4c4
AlertDev <49847759+alerta-dev@users.noreply.github.com>
2024-10-29 16:29:33 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,9 @@ import { remark } from 'remark';
 import html from 'remark-html';
 
 export async function getStaticProps() {
-  // Lee el archivo Markdown
   const filePath = path.join(process.cwd(), 'public', 'index.md');
   const fileContents = fs.readFileSync(filePath, 'utf8');
 
-  // Convierte el contenido a HTML
   const { content } = matter(fileContents);
   const processedContent = await remark().use(html).process(content);
   const contentHtml = processedContent.toString();
